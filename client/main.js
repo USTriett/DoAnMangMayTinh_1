f6b65c2d6941de2eaabbf3f974fb8bc482dc7dbf
USTriett <piglittle50@gmail.com>
2023-03-13 20:10:53 +0700

    add UI

--- client/main.js
+++ client/main.js
@@ -1,4 +1,9 @@
+var heartClicked = false;
+var voiceInput = false;
+var fileInput = false;
 
+var userText ='';
+var WhisperText ='';
 function getTime() {
   let today = new Date();
   hours = today.getHours();
@@ -31,17 +36,18 @@ const checkBrowserCompatibility = () => {
 
 'speechSynthesis' in window ? console.log("Web Speech API supported!") : console.log("Web Speech API not supported :-(")
 
-const synth = window.speechSynthesis
+const synth = window.speechSynthesis;
 
 
 
 function TextToSpeak(text)
 {
   let utterText = new SpeechSynthesisUtterance(text);
+  utterText.lang = "vi-VN";
   synth.speak(utterText);
 }
 
-
+var generate_prompt = "";
 function firstBotMessage() {
   let firstMessage = "How's it going?"
   document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
@@ -51,32 +57,62 @@ function firstBotMessage() {
   $("#chat-timestamp").append(time);
   document.getElementById("userInput").scrollIntoView(false);
 }
+
 //Gets the text text from the input box and processes it
+
+
+
+
+
+
 const getResponse = async(e) => {
-  let userText = $("#textInput").val();
-
-  if (userText == "") {
-      userText = "Hello";
+  userText = $("#textInput").val();
+  var userHtml = '';
+  if(fileInput == true)
+  {
+    fileInput == false;
+    userText = WhisperText
   }
-
-  let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+  else if(voiceInput == true)
+  {
+    userText = SpeechText;
+    voiceInput = false;
+  }
+  
+  else if (userText == "" && heartClicked == false && voiceInput == false && fileInput == false) {
+    userText = "Hello";
+  }
+  
+  if(heartClicked == false)
+    userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+  else if(heartClicked == true)
+  {
+      heartClicked = false;
+      userText = 'I Love You';
+  }
 
   $("#textInput").val("");
   $("#chatbox").append(userHtml);
   console.log(userText);
+  generate_prompt = generate_prompt + '\nYou' + userText;
   document.getElementById("chat-bar-bottom").scrollIntoView(true);
-  const response = await fetch('http://localhost:5000', {
-    headers: {
-      'Content-type': 'application/json'
-    },
+  const textResponse = await fetch('http://localhost:5000', {
     method: 'POST',
-    body: JSON.stringify({
-      prompt: userText
-    })
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: generate_prompt
+        })
   });
-  const data2 = await response.json();
-  const botResponse = data2.bot.trim();
-  console.log(data2);
+  
+  
+  // const data2 = await response.json();
+  // const botResponse = data2.bot.trim();
+  // console.log(data2);
+  const data = await textResponse.json();
+  const botResponse = data.bot.trim() // trims any trailing spaces/'\n' 
+  generate_prompt = generate_prompt + '\nEric: ' + botResponse;
   let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
   checkBrowserCompatibility();
   $("#chatbox").append(botHtml);
@@ -94,26 +130,110 @@ function buttonSendText(sampleText) {
   
   document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
-  //Uncomment this if you want the bot to respond to this buttonSendText event
-  // setTimeout(() => {
-  //     getHardResponse(sampleText);
-  // }, 1000)
 }
+
+
 
 function sendButton() {
   getResponse();
 }
 
 function heartButton() {
-  buttonSendText("Heart clicked!")
+  heartClicked = true;
+  buttonSendText('<i style="color: crimson;" class="fa fa-fw fa-heart"></i>');
+  getResponse("I Love You");
 }
 
 
-firstBotMessage();
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'en-US';
+recognition.continuous = false;
+
+const microphone = document.querySelector('.microphone');
+
+microphone.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  recognition.start();
+  microphone.classList.add('recording');
+
+});
+
+recognition.onspeechend = () => {
+  recognition.stop();
+  microphone.classList.remove('recording');
+}
+
+recognition.onerror = (err) => {
+  console.error(err);
+  microphone.classList.remove('recording');
+}
+
+recognition.onresult = (e) => {
+  console.log('onresult', e);
+  SpeechText = e.results[0][0].transcript;
+  voiceInput = true;
+  getResponse(e);
+}
+
+
+  var f = document.querySelector("#fileForm");
+  var btn = document.querySelector(".aufile");
+  btn.addEventListener("click", (e)=>{
+    if(f.style.display=="inline-block"){
+      f.style.display="none";
+
+      btn.style.position = "relative";
+      btn.style.zIndex = "0";
+      btn.style.right = "0";
+      btn.style.color = "black";
+    }
+    else{
+      f.style.display="inline-block";
+
+    btn.style.position = "absolute";
+    btn.style.zIndex = "11";
+    btn.style.right = "0";
+    btn.style.color = "red";
+    }
+  });
+  
+
+ 
+
+
+function submitForm(e, form){
+  e.preventDefault();
+  form.style.display = "none";
+  btn.style.position = "relative";
+  btn.style.zIndex = "0";
+  btn.style.right = "0";
+  btn.style.color = "black";
+  fetch('http://localhost:5000/up', {
+    method: 'post',
+    body: new FormData(form)
+  }).then(function(response) {
+    return response.json();
+    // fileInput = true;
+    // getResponse();
+  }).then((user)=>{
+    
+    WhisperText = user.bot;
+    fileInput = true;
+    getResponse();
+  });
+}
+
+
+//Main//
+firstBotMessage();
 // Press enter to send a message
 $("#textInput").keypress(function (e) {
   if (e.which == 13) {
       getResponse(e);
   }
 });
+
--- server/server.js
+++ server/server.js
@@ -2,12 +2,47 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
+import fs from 'fs';
+import path from "path";
+import axios from "axios";
 
-dotenv.config()
+import FormData from "form-data";
 
+const Key = "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";
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_API_KEY,
+  apiKey: "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",
 });
+
+
+import multer from "multer";
+
+var storage = multer.diskStorage({
+  destination : (req , file , res)=>{
+      res(null , '../audioUpload')
+  },
+  filename : (req , file , res)=>{
+      res(null , file.originalname )
+  }
+});
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+
+
+const model = 'whisper-1';
+
+
+// khai báo đối tượng multer
+// lưu trữ trên máy tính với 2 tham số định nghĩa ở trên destination , filename
+var upload = multer({ storage : storage });
+// gọi đến trang upload
+
+
+// const formData = new FormData();
 
 const openai = new OpenAIApi(configuration);
 
@@ -15,18 +50,57 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/up', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
+
+app.post('/up' , upload.single("filename") , async(req , res)=>{
+  // try {
+const text = req.body.request;
+const formData = new FormData();
+const filePath = path.join(__dirname, '../audioUpload', req.file.filename);
+formData.append("model", model);
+formData.append("file", fs.createReadStream(filePath));
+
+
+axios
+.post("https://api.openai.com/v1/audio/transcriptions", formData, {
+    headers:{
+        Authorization:`Bearer ${Key}`,
+        "Content-type": `multipath/form-data; boundary=${formData._boundary}`,
+    }
+})
+.then((response)=>{
+  console.log("Speech-to-text");
+  
+    res.status(200).send({
+    bot: response.data.text
+  })
+  // fs.unlinkSync(filePath);
+})
+  // } catch (error) {
+  //   console.error(error)
+  //   res.status(500).send(error || 'Something went wrong');
+  // }
+
+} );
+
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from CodeX!'
   })
 })
 
+
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
+    
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "The following is the conversation between USER and ERIC. ERIC is an intelligent, rude friend. Eric is a fan of Neymar JR.\nYou: Hello\nEric: Hey what's up\nYou: Who are you?\nEric: I'm Eric, nice to meet you. What can I help you with?" + `${prompt}` + '\nEric:',
+        prompt: "Conversation between User and Eric. Eric is an intelligent chatbot who is a teenage love expert. Eric can understand emotional feeling and sympathize with the breakup. Eric can understand and reply by Vietnamese.\nYou: Hello\nEric: Hey what's up\nYou: Who are you?\nEric: I'm Eric, nice to meet you. What can I help you with?" + `${prompt}` + '\nEric:',
         temperature: 0.9,
         max_tokens: 256,
         top_p: 1,
